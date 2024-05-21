1c7ed12b531dbb615da810cc08404b2084e84443
Sattineez_SC <83277661+SaraSSC@users.noreply.github.com>
2024-05-21 20:55:30 +0100

    Code and the pictures for the background an UI

---
+++ MaquinaDeBebidas/Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace MaquinaDeBebidas
+{
+    partial class FormMaquinaDeBebidas
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMaquinaDeBebidas));
+            this.lblTotalInserido = new System.Windows.Forms.Label();
+            this.btnMoeda05 = new System.Windows.Forms.Button();
+            this.btnMoeda10 = new System.Windows.Forms.Button();
+            this.btnMoeda20 = new System.Windows.Forms.Button();
+            this.btnMoeda50 = new System.Windows.Forms.Button();
+            this.btnMoeda1 = new System.Windows.Forms.Button();
+            this.btnMoeda2 = new System.Windows.Forms.Button();
+            this.btnCafe = new System.Windows.Forms.Button();
+            this.btnCappuccino = new System.Windows.Forms.Button();
+            this.btnChocolate = new System.Windows.Forms.Button();
+            this.btnCha = new System.Windows.Forms.Button();
+            this.label1Passo = new System.Windows.Forms.Label();
+            this.label2Passo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblTotalInserido
+            // 
+            this.lblTotalInserido.AutoSize = true;
+            this.lblTotalInserido.BackColor = System.Drawing.Color.Black;
+            this.lblTotalInserido.Font = new System.Drawing.Font("Ink Free", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotalInserido.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblTotalInserido.Location = new System.Drawing.Point(471, 151);
+            this.lblTotalInserido.Name = "lblTotalInserido";
+            this.lblTotalInserido.Size = new System.Drawing.Size(218, 37);
+            this.lblTotalInserido.TabIndex = 1;
+            this.lblTotalInserido.Text = "Total Inserido";
+            // 
+            // btnMoeda05
+            // 
+            this.btnMoeda05.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnMoeda05.BackgroundImage")));
+            this.btnMoeda05.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnMoeda05.Font = new System.Drawing.Font("Ink Free", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMoeda05.Location = new System.Drawing.Point(12, 91);
+            this.btnMoeda05.Name = "btnMoeda05";
+            this.btnMoeda05.Size = new System.Drawing.Size(89, 84);
+            this.btnMoeda05.TabIndex = 2;
+            this.btnMoeda05.Text = "0.05€";
+            this.btnMoeda05.UseVisualStyleBackColor = true;
+            this.btnMoeda05.Click += new System.EventHandler(this.btnMoeda05_Click);
+            // 
+            // btnMoeda10
+            // 
+            this.btnMoeda10.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnMoeda10.BackgroundImage")));
+            this.btnMoeda10.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnMoeda10.Font = new System.Drawing.Font("Ink Free", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMoeda10.Location = new System.Drawing.Point(12, 197);
+            this.btnMoeda10.Name = "btnMoeda10";
+            this.btnMoeda10.Size = new System.Drawing.Size(89, 86);
+            this.btnMoeda10.TabIndex = 3;
+            this.btnMoeda10.Text = "0.10€";
+            this.btnMoeda10.UseVisualStyleBackColor = true;
+            this.btnMoeda10.Click += new System.EventHandler(this.btnMoeda10_Click);
+            // 
+            // btnMoeda20
+            // 
+            this.btnMoeda20.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnMoeda20.BackgroundImage")));
+            this.btnMoeda20.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnMoeda20.Font = new System.Drawing.Font("Ink Free", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMoeda20.Location = new System.Drawing.Point(12, 301);
+            this.btnMoeda20.Name = "btnMoeda20";
+            this.btnMoeda20.Size = new System.Drawing.Size(89, 89);
+            this.btnMoeda20.TabIndex = 4;
+            this.btnMoeda20.Text = "0.20€";
+            this.btnMoeda20.UseVisualStyleBackColor = true;
+            this.btnMoeda20.Click += new System.EventHandler(this.btnMoeda20_Click);
+            // 
+            // btnMoeda50
+            // 
+            this.btnMoeda50.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnMoeda50.BackgroundImage")));
+            this.btnMoeda50.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnMoeda50.Font = new System.Drawing.Font("Ink Free", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMoeda50.Location = new System.Drawing.Point(12, 407);
+            this.btnMoeda50.Name = "btnMoeda50";
+            this.btnMoeda50.Size = new System.Drawing.Size(107, 84);
+            this.btnMoeda50.TabIndex = 5;
+            this.btnMoeda50.Text = "0.50€";
+            this.btnMoeda50.UseVisualStyleBackColor = true;
+            this.btnMoeda50.Click += new System.EventHandler(this.btnMoeda50_Click);
+            // 
+            // btnMoeda1
+            // 
+            this.btnMoeda1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnMoeda1.BackgroundImage")));
+            this.btnMoeda1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnMoeda1.Font = new System.Drawing.Font("Ink Free", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMoeda1.Location = new System.Drawing.Point(134, 316);
+            this.btnMoeda1.Name = "btnMoeda1";
+            this.btnMoeda1.Size = new System.Drawing.Size(107, 85);
+            this.btnMoeda1.TabIndex = 6;
+            this.btnMoeda1.Text = "1€";
+            this.btnMoeda1.UseVisualStyleBackColor = true;
+            this.btnMoeda1.Click += new System.EventHandler(this.btnMoeda1_Click);
+            // 
+            // btnMoeda2
+            // 
+            this.btnMoeda2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnMoeda2.BackgroundImage")));
+            this.btnMoeda2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnMoeda2.Font = new System.Drawing.Font("Ink Free", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMoeda2.Location = new System.Drawing.Point(134, 407);
+            this.btnMoeda2.Name = "btnMoeda2";
+            this.btnMoeda2.Size = new System.Drawing.Size(107, 89);
+            this.btnMoeda2.TabIndex = 7;
+            this.btnMoeda2.Text = "2€";
+            this.btnMoeda2.UseVisualStyleBackColor = true;
+            this.btnMoeda2.Click += new System.EventHandler(this.btnMoeda2_Click);
+            // 
+            // btnCafe
+            // 
+            this.btnCafe.Font = new System.Drawing.Font("Ink Free", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCafe.Location = new System.Drawing.Point(490, 385);
+            this.btnCafe.Name = "btnCafe";
+            this.btnCafe.Size = new System.Drawing.Size(122, 27);
+            this.btnCafe.TabIndex = 8;
+            this.btnCafe.Text = "Café";
+            this.btnCafe.UseVisualStyleBackColor = true;
+            this.btnCafe.Click += new System.EventHandler(this.btnCafe_Click);
+            // 
+            // btnCappuccino
+            // 
+            this.btnCappuccino.Font = new System.Drawing.Font("Ink Free", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCappuccino.Location = new System.Drawing.Point(490, 329);
+            this.btnCappuccino.Name = "btnCappuccino";
+            this.btnCappuccino.Size = new System.Drawing.Size(128, 38);
+            this.btnCappuccino.TabIndex = 9;
+            this.btnCappuccino.Text = "Cappuccino";
+            this.btnCappuccino.UseVisualStyleBackColor = true;
+            this.btnCappuccino.Click += new System.EventHandler(this.btnCappuccino_Click);
+            // 
+            // btnChocolate
+            // 
+            this.btnChocolate.Font = new System.Drawing.Font("Ink Free", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnChocolate.Location = new System.Drawing.Point(491, 231);
+            this.btnChocolate.Name = "btnChocolate";
+            this.btnChocolate.Size = new System.Drawing.Size(127, 36);
+            this.btnChocolate.TabIndex = 10;
+            this.btnChocolate.Text = "Chocolate";
+            this.btnChocolate.UseVisualStyleBackColor = true;
+            this.btnChocolate.Click += new System.EventHandler(this.btnChocolate_Click);
+            // 
+            // btnCha
+            // 
+            this.btnCha.Font = new System.Drawing.Font("Ink Free", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCha.Location = new System.Drawing.Point(490, 273);
+            this.btnCha.Name = "btnCha";
+            this.btnCha.Size = new System.Drawing.Size(128, 34);
+            this.btnCha.TabIndex = 11;
+            this.btnCha.Text = "Chá";
+            this.btnCha.UseVisualStyleBackColor = true;
+            this.btnCha.Click += new System.EventHandler(this.btnCha_Click);
+            // 
+            // label1Passo
+            // 
+            this.label1Passo.AutoSize = true;
+            this.label1Passo.Font = new System.Drawing.Font("Ink Free", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1Passo.Location = new System.Drawing.Point(7, 9);
+            this.label1Passo.Name = "label1Passo";
+            this.label1Passo.Size = new System.Drawing.Size(231, 29);
+            this.label1Passo.TabIndex = 12;
+            this.label1Passo.Text = "1º Insere as moedas";
+            // 
+            // label2Passo
+            // 
+            this.label2Passo.AutoSize = true;
+            this.label2Passo.Font = new System.Drawing.Font("Ink Free", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2Passo.Location = new System.Drawing.Point(12, 50);
+            this.label2Passo.Name = "label2Passo";
+            this.label2Passo.Size = new System.Drawing.Size(223, 29);
+            this.label2Passo.TabIndex = 13;
+            this.label2Passo.Text = "2º Escolhe a bebida";
+            // 
+            // FormMaquinaDeBebidas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Plum;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(884, 518);
+            this.Controls.Add(this.label2Passo);
+            this.Controls.Add(this.label1Passo);
+            this.Controls.Add(this.btnCha);
+            this.Controls.Add(this.btnChocolate);
+            this.Controls.Add(this.btnCappuccino);
+            this.Controls.Add(this.btnCafe);
+            this.Controls.Add(this.btnMoeda2);
+            this.Controls.Add(this.btnMoeda1);
+            this.Controls.Add(this.btnMoeda50);
+            this.Controls.Add(this.btnMoeda20);
+            this.Controls.Add(this.btnMoeda10);
+            this.Controls.Add(this.btnMoeda05);
+            this.Controls.Add(this.lblTotalInserido);
+            this.DoubleBuffered = true;
+            this.Name = "FormMaquinaDeBebidas";
+            this.Text = "FormMaquinaDeBebidas";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblTotalInserido;
+        private System.Windows.Forms.Button btnMoeda05;
+        private System.Windows.Forms.Button btnMoeda10;
+        private System.Windows.Forms.Button btnMoeda20;
+        private System.Windows.Forms.Button btnMoeda50;
+        private System.Windows.Forms.Button btnMoeda1;
+        private System.Windows.Forms.Button btnMoeda2;
+        private System.Windows.Forms.Button btnCafe;
+        private System.Windows.Forms.Button btnCappuccino;
+        private System.Windows.Forms.Button btnChocolate;
+        private System.Windows.Forms.Button btnCha;
+        private System.Windows.Forms.Label label1Passo;
+        private System.Windows.Forms.Label label2Passo;
+    }
+}
+
---
+++ MaquinaDeBebidas/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MaquinaDeBebidas
+{
+    public partial class FormMaquinaDeBebidas : Form
+    {
+
+
+        // Dicionário para mapear o valor de cada bebida
+        Dictionary<string, double> precos = new Dictionary<string, double>()
+        {
+            { "Café", 0.25 },
+            { "Cappuccino", 0.30 },
+            { "Chocolate", 0.35 },
+            { "Chá", 0.20 }
+        };
+
+        // Dicionário para mapear o valor de cada moeda
+        Dictionary<double, int> moedas = new Dictionary<double, int>()
+        {
+            { 0.05, 10 },
+            { 0.10, 10 },
+            { 0.20, 10 },
+            { 0.50, 10 },
+            { 1.00, 10 },
+            { 2.00, 10 }
+        };
+
+        // Variável para armazenar o valor total inserido
+        double totalInserido = 0;
+
+        public FormMaquinaDeBebidas()
+        {
+            InitializeComponent();
+        }
+
+        // Método para atualizar o display do total inserido
+        private void AtualizarDisplay()
+        {
+            lblTotalInserido.Text = totalInserido.ToString("C");
+        }
+
+        // Método para processar a inserção de moedas
+        private void InserirMoeda(double valorMoeda)
+        {
+            totalInserido += valorMoeda;
+            AtualizarDisplay();
+        }
+
+        // Método para processar a compra de uma bebida
+        private void ComprarBebida(string bebida, double preco)
+        {
+            if (totalInserido >= preco)
+            {
+                // Calcula o troco
+                double troco = totalInserido - preco;
+                totalInserido = 0;
+
+                // Mostra o troco
+                MessageBox.Show($"Aqui está sua bebida: {bebida}. Troco: {troco.ToString("C")}");
+
+                // Calcula e exibe as moedas do troco
+                var moedasOrdenadas = moedas.Keys.OrderByDescending(v => v);
+                foreach (var valorMoeda in moedasOrdenadas)
+                {
+                    int quantidade = (int)(troco / valorMoeda);
+                    troco -= quantidade * valorMoeda;
+                    if (quantidade > 0)
+                    {
+                        MessageBox.Show($"Troco: {quantidade} moeda(s) de {valorMoeda.ToString("C")}");
+                    }
+                }
+
+                AtualizarDisplay();
+            }
+            else
+            {
+                MessageBox.Show("Dinheiro insuficiente.");
+            }
+            
+        }
+
+        // Eventos dos botões de moedas
+        private void btnMoeda05_Click(object sender, EventArgs e)
+        {
+            InserirMoeda(0.05);
+        }
+
+        private void btnMoeda10_Click(object sender, EventArgs e)
+        {
+            InserirMoeda(0.10);
+        }
+
+        private void btnMoeda20_Click(object sender, EventArgs e)
+        {
+            InserirMoeda(0.20);
+        }
+
+        private void btnMoeda50_Click(object sender, EventArgs e)
+        {
+            InserirMoeda(0.50);
+        }
+
+        private void btnMoeda1_Click(object sender, EventArgs e)
+        {
+            InserirMoeda(1.00);
+        }
+
+        private void btnMoeda2_Click(object sender, EventArgs e)
+        {
+            InserirMoeda(2.00);
+        }
+
+        // Eventos dos botões de bebidas
+        private void btnCafe_Click(object sender, EventArgs e)
+        {
+            ComprarBebida("Café", precos["Café"]);
+        }
+
+        private void btnCappuccino_Click(object sender, EventArgs e)
+        {
+            ComprarBebida("Cappuccino", precos["Cappuccino"]);
+        }
+
+        private void btnChocolate_Click(object sender, EventArgs e)
+        {
+            ComprarBebida("Chocolate", precos["Chocolate"]);
+        }
+
+        private void btnCha_Click(object sender, EventArgs e)
+        {
+            ComprarBebida("Chá", precos["Chá"]);
+        }
+
+        
+        }
+}
+
